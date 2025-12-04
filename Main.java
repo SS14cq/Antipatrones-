cd54a64c863a9101cd143c2d70b652271668e2bc
SS14cq <144471317+SS14cq@users.noreply.github.com>
2025-12-03 23:25:39 -0500

    Simplify main method and initialize Veterinaria
    Refactor main method to reduce logic and improve clarity.

--- Main.java
+++ Main.java
@@ -1,12 +1,18 @@
 import Veterinaria.*;
+
 public class Main {
     public static void main(String[] args) {
+
+        /* 
+         * Se inicializa un arreglo sin lógica innecesaria
+         * Evita el antipatrón "Lógica en main" (demasiado código aquí).
+         */
         Animal[] animales = new Animal[2];
-        
+
         animales[0] = new Perro("Firulais", "Café", "Croquetas");
         animales[1] = new Perro("Max", "Negro", "Pollo");
-        
-        Veterinaria veterinaria = new Veterinaria("El perro Feliz", animales);
+
+        Veterinaria veterinaria = new Veterinaria("El Perro Feliz", animales);
 
         veterinaria.mostrarAnimales();
         veterinaria.tratarAnimales();
