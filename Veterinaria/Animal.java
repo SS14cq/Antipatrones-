fb01f046a9ac89b2494d4fe3b4f5bc786fee9b71
SS14cq <144471317+SS14cq@users.noreply.github.com>
2025-12-03 23:22:57 -0500

    Refactor Animal class con comentarios

--- Veterinaria/Animal.java
+++ Veterinaria/Animal.java
@@ -1,38 +1,53 @@
 package Veterinaria;
 import java.util.Random;
+import java.util.Scanner;
 
 public abstract class Animal {
 
-    public enum Padecimiento {
+    /* 
+     * Enum correcto para evitar usar cadenas sueltas
+     * Evita el antipatrón: "Magic Strings"
+     */
+    public enum padecimientos {
         GRIPE, FRACTURA, INFECCION, OTRO
     }
 
     protected String nombre, colorPelaje, comidaFavorita;
-    protected Padecimiento padecimiento;
+    protected padecimientos padecimiento;
+
+    /*
+     * Se evita cerrar este Scanner en clases hijas
+     * Antes: antipatrón Resource Leak y cerrar System.in accidentalmente.
+     */
+    Scanner scanner = new Scanner(System.in);
+
+    public abstract void registrarAnimal();
+    public abstract padecimientos revisarAnimal();
+    public abstract void tratarAnimal();
 
     public Animal(String nombre, String colorPelaje, String comidaFavorita) {
         this.nombre = nombre;
         this.colorPelaje = colorPelaje;
         this.comidaFavorita = comidaFavorita;
+
+        /* 
+         * Se asigna un padecimiento automáticamente
+         * Esto evita el antipatrón de dejar atributos sin inicializar.
+         */
         asignarPadecimientoAleatorio();
     }
 
-    public abstract void tratarAnimal();
-
     public void mostrarInfoBasica() {
+
         System.out.println("\nNombre: " + nombre);
         System.out.println("Color de Pelaje: " + colorPelaje);
         System.out.println("Comida Favorita: " + comidaFavorita);
-        System.out.println("Padecimiento: " + padecimiento + "\n");
+        System.out.println("El animal presenta el siguiente padecimiento: " + padecimiento + "\n" );
     }
 
-    private void asignarPadecimientoAleatorio() {
+    public void asignarPadecimientoAleatorio() {
         Random random = new Random();
-        Padecimiento[] valores = Padecimiento.values();
+        padecimientos[] valores = padecimientos.values();
         this.padecimiento = valores[random.nextInt(valores.length)];
-    }
-
-    public Padecimiento getPadecimiento() {
-        return padecimiento;
     }
 }
