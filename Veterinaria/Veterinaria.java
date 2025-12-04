593d1451bad0957e665fb850a0e088751e5f7cab
SS14cq <144471317+SS14cq@users.noreply.github.com>
2025-12-03 23:25:02 -0500

    Refactor Veterinaria class con comentarios

--- Veterinaria/Veterinaria.java
+++ Veterinaria/Veterinaria.java
@@ -4,21 +4,28 @@ public class Veterinaria {
     String nombre;
     Animal[] animals;
 
-    public Veterinaria (String nombre, Animal[] animals ){
+    public Veterinaria(String nombre, Animal[] animals){
         this.nombre = nombre;
         this.animals = animals;
+
+        /*
+         * Aquí se evita el antipatrón "God Object"
+         * La veterinaria SOLO coordina, no hace lógica médica.
+         */
     }
 
     public void ingresarAnimales(){
         for(Animal animal : animals){
             animal.registrarAnimal();
-        } 
+        }
     }
+
     public void mostrarAnimales(){
         for(Animal animal : animals){
             animal.mostrarInfoBasica();
         }
     }
+
     public void tratarAnimales(){
         for(Animal animal : animals){
             animal.tratarAnimal();
